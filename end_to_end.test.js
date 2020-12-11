7a69430270f285552e118327bd90dbb085dc1c2b
Ryan Powell <ryan.powell.hkrctr@gmail.com>
2020-12-11 10:24:39 -0800

    add story, test fails title test

--- end_to_end.test.js
+++ end_to_end.test.js
@@ -1,7 +1,10 @@
 const { chromium } = require('playwright');
 const app = require('./api/server.js');
+
 let browser;
 let page;
+let server;
+
 beforeAll(async () => {
   server = app.listen(3000)
   browser = await chromium.launch();
@@ -16,7 +19,34 @@ beforeEach(async () => {
 afterEach(async () => {
   await page.close();
 });
-it('server smoke test', async () => {
+
+
+it('Page title is "To-Do"', async () => {
+	// Edith has heard about a cool new online to-do app. She goes
+	// to check out its homepage
   await page.goto('http://localhost:3000');
-  expect(await page.title()).toBe('Hello, World!');
+	// She notices the page title and header mention to-do lists
+	// assert 'To-Do' in browser.title
+  expect(await page.title()).toBe('To-Do');
 });
+//
+// # She is invited to enter a to-do item straight away
+//
+// # She types "Buy peacock feathers" into a text box (Edith's hobby
+// # is tying fly-fishing lures)
+//
+// # When she hits enter, the page updates, and now the page lists
+// # "1: Buy peacock feathers" as an item in a to-do list
+//
+// # There is still a text box inviting her to add another item. She
+// # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+//
+// # The page updates again, and now shows both items on her list
+//
+// # Edith wonders whether the site will remember her list. Then she sees
+// # that the site has generated a unique URL for her -- there is some
+// # explanatory text to that effect.
+//
+// # She visits that URL - her to-do list is still there.
+//
+// # Satisfied, she goes back to sleep

9edceb0fdba9ac26c050678b765a14f86518a595
Ryan Powell <ryan.powell.hkrctr@gmail.com>
2020-12-11 10:37:54 -0800

    move start.js to index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,2 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/test', async (req, res) => {
-  res.json({message: 'pass!'});
-});
-
-app.use(express.static(path.join(__dirname, '../client/public')));
-
-module.exports = app;
-
-if (require.main === module) {
- app.listen(port);
-}
+const app = require('./server.js')
+app.listen(3000)

1ff545b300afa298927e88f58dc08086799459a4
Ryan Powell <ryan.powell.hkrctr@gmail.com>
2020-12-12 03:35:29 -0800

    initialize npm in api and client dirs

--- end_to_end_tests/end_to_end.test.js
+++ end_to_end_tests/end_to_end.test.js
@@ -20,35 +20,41 @@ afterEach(async () => {
   await page.close();
 });
 
-
-it('Page title is "To-Do"', async () => {
-	// Edith has heard about a cool new online to-do app. She goes
-	// to check out its homepage
-  await page.goto('http://localhost:3000');
-	// She notices the page title and header mention to-do lists
-	// assert 'To-Do' in browser.title
-  expect(await page.title()).toBe('To-Do');
+describe("Test can start a list and retrieve it later", () => {
+  it('Page title is "To-Do"', async () => {
+    // Edith has heard about a cool new online to-do app. She goes
+    // to check out its homepage
+    await page.goto('http://localhost:3000');
+    // She notices the page title and header mention to-do lists
+    // assert 'To-Do' in browser.title
+    expect(await page.title()).toBe('To-Do');
+  });
+  // # She is invited to enter a to-do item straight away
+  it('Input Box Has Placeholder Text', async () => {
+    await page.goto('http://localhost:3000');
+    const placeholderText = await page.$eval(
+      '#id_new_item',
+      el => el.placeholder
+    );
+    expect(placeholderText).toEqual('Enter a to-do item');
+  });
+  //
+  // She types "Buy peacock feathers" into a text box (Edith's hobby
+  // is tying fly-fishing lures)
+  //
+  // When she hits enter, the page updates, and now the page lists
+  // "1: Buy peacock feathers" as an item in a to-do list
+  //
+  // There is still a text box inviting her to add another item. She
+  // enters "Use peacock feathers to make a fly" (Edith is very methodical)
+  //
+  // The page updates again, and now shows both items on her list
+  //
+  // Edith wonders whether the site will remember her list. Then she sees
+  // that the site has generated a unique URL for her -- there is some
+  // explanatory text to that effect.
+  //
+  // She visits that URL - her to-do list is still there.
+  //
+  // Satisfied, she goes back to sleep
 });
-// # She is invited to enter a to-do item straight away
-it('Finish the Test!', () => {
-  throw new Error('Finish the Test!');
-});
-//
-// # She types "Buy peacock feathers" into a text box (Edith's hobby
-// # is tying fly-fishing lures)
-//
-// # When she hits enter, the page updates, and now the page lists
-// # "1: Buy peacock feathers" as an item in a to-do list
-//
-// # There is still a text box inviting her to add another item. She
-// # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-//
-// # The page updates again, and now shows both items on her list
-//
-// # Edith wonders whether the site will remember her list. Then she sees
-// # that the site has generated a unique URL for her -- there is some
-// # explanatory text to that effect.
-//
-// # She visits that URL - her to-do list is still there.
-//
-// # Satisfied, she goes back to sleep
--- todo.md
+++ todo.md
@@ -1,1 +1,4 @@
 figure out how to select which tests get ran with playwright
+continued @ chapter 4
+
+`“Finish the test”, it says, so let’s do just that! Open up functional_tests.py and we’ll extend our FT:`

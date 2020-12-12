c02bfb34fed9a7146a097d1a57f8962943790633
Ryan Powell <ryan.powell.hkrctr@gmail.com>
2020-12-12 04:15:53 -0800

    empty App test and webpack config

---
+++ client/src/App.test.js
@@ -0,0 +1,8 @@
+const React = require('react');
+const { mount } = require('enzyme');
+
+const App = require('./App');
+
+describe('App', () => {
+  it('', () => {});
+});
---
+++ client/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  resolve: { extensions: ['.js'] },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        loader:'style-loader!css-loader'
+      },
+    ],
+  },
+};
